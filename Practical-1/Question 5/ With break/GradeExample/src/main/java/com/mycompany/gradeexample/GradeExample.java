609ea31d7e5e61aa5e5ba2e87f131325ba434a74
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 12:08:18 +0530

    Add files via upload

---
+++ Practical-1/Question 5/ With break/GradeExample/src/main/java/com/mycompany/gradeexample/GradeExample.java
@@ -0,0 +1,20 @@
+package com.mycompany.gradeexample;
+public class GradeExample {
+    public static void main(String[] args) {
+        char grade = 'A';
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'D':
+                System.out.println("You passed");
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
+
---
+++ Practical-1/Question 5/Using if-else-if statement/GradeExample/src/main/java/com/mycompany/gradeexample/GradeExample.java
@@ -0,0 +1,17 @@
+package com.mycompany.gradeexample;
+public class GradeExample {
+    public static void main(String[] args) {
+        char grade = 'A';
+        if (grade == 'A') {
+            System.out.println("Excellent!");
+        } else if (grade == 'D') {
+            System.out.println("You passed");
+        } else if (grade == 'F') {
+            System.out.println("Better try again");
+        } else {
+            System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
+
