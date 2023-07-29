40d43ae25c5d75f6a0bbce358bca2c38edf22fc8
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 11:04:14 +0530

    Add files via upload

---
+++ Practical-1/PersonalInfoApp/src/main/java/com/mycompany/personalinfoapp/PersonalInfoApp.java
@@ -0,0 +1,10 @@
+package com.mycompany.personalinfoapp;
+public class PersonalInfoApp {
+    public static void main(String[] args) {
+        String name = "L R S S Bandara";
+        String degreeProgram = "Management Information Systems(MIS)";
+        
+        System.out.println(name);
+        System.out.println(degreeProgram);
+    }
+}
