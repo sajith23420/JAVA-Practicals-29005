0feb8dde1c5ceca66552fb051e68679449416aec
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 11:08:18 +0530

    Delete Practical-1/ForLoopExample directory

--- Practical-1/ForLoopExample/src/main/java/com/mycompany/forloopexample/ForLoopExample.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.forloopexample;
-public class ForLoopExample 
-{
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Executing Loop " + i);
-        }
-    }
-}
