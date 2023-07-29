94c752cefe99d1a22549c74198f259a879b54533
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 11:05:12 +0530

    Add files via upload

---
+++ Practical-1/WhileLoopExample/src/main/java/com/mycompany/whileloopexample/WhileLoopExample.java
@@ -0,0 +1,11 @@
+package com.mycompany.whileloopexample;
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Executing Loop " + i);
+            i++;
+        }
+    }
+}
+
