d1d6b3d3c3eb43124023eaa454d8d4fe4b041075
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 11:11:11 +0530

    Add files via upload

---
+++ Practical-1/Question 3/ForLoopExample/src/main/java/com/mycompany/forloopexample/ForLoopExample.java
@@ -0,0 +1,9 @@
+package com.mycompany.forloopexample;
+public class ForLoopExample 
+{
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Executing Loop " + i);
+        }
+    }
+}
