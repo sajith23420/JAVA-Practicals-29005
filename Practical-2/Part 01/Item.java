f819fc7fad91ccacc79ceda528c7280e9f3edb80
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 13:20:40 +0530

    Add files via upload

---
+++ Practical-2/Part 01/Item.java
@@ -0,0 +1,27 @@
+package com.mycompany.itemmain;
+public class Item {
+    
+    private int location;
+    private String description;
+  
+    public Item(int location, String description) {
+        this.location = location;
+        this.description = description;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Practical-2/Part 01/Monster.java
@@ -0,0 +1,6 @@
+public class Monster extends Item {
+   
+    public Monster(int location, String description) {
+        super(location, description); 
+    }
+}
