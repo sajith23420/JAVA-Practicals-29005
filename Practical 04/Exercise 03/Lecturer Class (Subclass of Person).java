c34de998fd07f41cccf9ce88e4a446f948059ecd
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 17:36:27 +0530

    Add files via upload

---
+++ Practical 04/Exercise 03/Lecturer Class (Subclass of Person).java
@@ -0,0 +1,12 @@
+public class Lecturer extends Person {
+    private String programme;
+
+    // Getter and Setter methods for programme
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+}
---
+++ Practical 04/Exercise 03/Students Class(Subclass of Person).java
@@ -0,0 +1,12 @@
+public class Student extends Person {
+    private String course;
+
+    // Getter and Setter methods for course
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
