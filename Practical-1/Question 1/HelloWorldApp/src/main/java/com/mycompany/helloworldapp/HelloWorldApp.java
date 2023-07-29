58042370c7347baddef545bfe2c822fc0263b430
sajith23420 <140834328+sajith23420@users.noreply.github.com>
2023-07-29 11:10:06 +0530

    Add files via upload

---
+++ Practical-1/Question 1/HelloWorldApp/src/main/java/com/mycompany/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.helloworldapp;
+
+/**
+ *
+ * @author sajit
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
